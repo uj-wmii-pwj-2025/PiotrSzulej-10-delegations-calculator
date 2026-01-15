77a8cfc4f855fea252a7163d56da3bb097ca80f6
Piotr Szulej <piodomsz@gmail.com>
2026-01-15 23:49:20 +0100

    solution

--- src/main/java/uj/wmii/pwj/delegations/Calc.java
+++ src/main/java/uj/wmii/pwj/delegations/Calc.java
@@ -7,8 +7,33 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Calc {
-
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z");
     BigDecimal calculate(String name, String start, String end, BigDecimal dailyRate) throws IllegalArgumentException {
-        return null;
+        ZonedDateTime startTime = ZonedDateTime.parse(start, FORMATTER);
+        ZonedDateTime endTime = ZonedDateTime.parse(end, FORMATTER);
+
+        if (startTime.isAfter(endTime) || startTime.isEqual(endTime)) {
+            return BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+        }
+
+        Duration duration = Duration.between(startTime, endTime);
+        long totalMinutes = duration.toMinutes();
+
+        long fullDays = totalMinutes / 1440;
+        long remainingMinutes = totalMinutes % 1440;
+
+        BigDecimal totalAmount = dailyRate.multiply(BigDecimal.valueOf(fullDays));
+
+        if (remainingMinutes > 0) {
+            if (remainingMinutes <= 480) {
+                totalAmount = totalAmount.add(dailyRate.divide(BigDecimal.valueOf(3), 2, RoundingMode.HALF_UP));
+            } else if (remainingMinutes <= 720) {
+                totalAmount = totalAmount.add(dailyRate.divide(BigDecimal.valueOf(2), 2, RoundingMode.HALF_UP));
+            } else {
+                totalAmount = totalAmount.add(dailyRate);
+            }
+        }
+
+        return totalAmount.setScale(2, RoundingMode.HALF_UP);
     }
 }
